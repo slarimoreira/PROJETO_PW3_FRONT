0ab98057e32645420a805aa8b38562e5d7c48b1d
Larissa Moreira <larissamoreirasilva28@gmail.com>
2022-12-01 16:58:40 -0300

    DELETANDO ANOTAÇÃO

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const axios = require('axios').default;
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -9,52 +10,108 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/anotacao', (req, res)=>{
-    res.render('anotacao/index');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('listagem', (req, res) =>{
+
 });
 
-app.get('/listagem', (req, res)=>{
+
+// app.get('/listagem', (req, res)=>{
     
-    const urlListagemAnotacao = 'http://localhost:3000/listarAnotacao';
+//     const urlListagemAnotacao = 'http://localhost:3000/listarAnotacao';
 
-    axios.get(urlListagemAnotacao)
-        .then(
-            (response)=>{
+//     axios.get(urlListagemAnotacao)
+//         .then(
+//             (response)=>{
 
-                let anotacao = response.data;
-                res.render('anotacao/listagem', {anotacao});
+//                 let anotacao = response.data;
+//                 res.render('anotacao/listagem', {anotacao});
 
-        }); 
+//         }); 
+// });
+
+
+    app.get('/listagem', (req, res)=>{
+        
+        const urlListagemAnotacao = 'http://localhost:3000/listarAnotacao';
+
+                axios.get(urlListagemAnotacao)
+                .then((response)=>{
+
+                    let anotacao = response.data;
+                    res.render('anotacao/listagem', {anotacao});
+
+                    const inserirAnotacao = 'http://localhost:3000/inserirAnotacao';
+
+                    axios.post(inserirAnotacao)
+                    .then(()=>{
+
+                         console.log('FUncionou')       
+
+                    }); 
+                });
+
+        
     });
 
-    app.get('/formEdicaoCategorias/:id', (req, res)=>{
+    app.get('/listagem/:id', (req, res)=>{
 
         let {id} = req.params;
 
-        const urlListagemCategoria = `http://localhost:3000/listarCategoria/${id}`;
-        
-        axios.get(urlListagemCategoria)
-        .then(
-            (response)=>{
+        console.log(id)
 
-                let categoria = response.data;
-                res.render('categoria/editarCategoria', {categoria});
+        const urlListagemAnotacao = `http://localhost:3000/listarNota/${id}`;
+        
+        axios.get(urlListagemAnotacao)
+        .then(
+            (response)=>{   
+
+                let anotacao = response.data;
+                res.render('anotacao/listagem', {anotacao});
 
             }
         )
     });
 
-    app.post('/alterarCategoria', (req, res)=>{
 
-        const urlAlterarCategoria = 'http://localhost:3000/alterarCategoria';
-        console.log(req.body);
+    app.get('/deletarListagem/:id', (req, res)=>{
 
-        axios.put(urlAlterarCategoria, req.body)
+        let {id} = req.params;
+
+        const deleteAnotacao = `http://localhost:3000/excluirNota/${id}`;
+        
+        axios.delete(deleteAnotacao)
         .then(
-            res.send('ALTERADO!')
-        )
+            (response)=>{   
 
+                const urlListagemAnotacao = 'http://localhost:3000/listarAnotacao';
+
+                axios.get(urlListagemAnotacao)
+                    .then((response)=>{
+
+                        let anotacao = response.data;
+                        res.render('anotacao/listagem', {anotacao});
+
+                    });
+
+            }
+        )
     });
+
+
+    // app.post('/alterarCategoria', (req, res)=>{
+
+    //     const urlListagemAnotacao = 'http://localhost:3000/alterarCategoria';
+    //     console.log(req.body);
+
+    //     axios.put(urlListagemAnotacao, req.body)
+    //     .then(
+    //         res.send('ALTERADO!')
+    //     )
+
+    // });
 
 app.listen(8070, ()=>{
     console.log('SERVIDOR RODANDO EM: http://localhost:8070');
